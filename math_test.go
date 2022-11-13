9ac39733704560874bd0af66743eff57b845438f
gustavo torregrosa <gustavo.torregrosa@gmail.com>
2022-11-12 23:33:56 -0300

    ci: fix test

--- math_test.go
+++ math_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSoma(t *testing.T) {
-	total := 15 + 16	
+	total := 15 + 15	
 	if total != 30 {
 		t.Errorf("Resultado da soma invalido. Resultado: %d, Esperado: %d", total, 30)
 	}
